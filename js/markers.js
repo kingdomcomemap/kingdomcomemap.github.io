3a882f37c4c3dd5956e1a494e1c0da9569b3b75e
rogerhnn <rogerhnn@hotmail.com>
2018-03-03 09:25:28 -0300

    Fixed Icons Url

--- js/markers.js
+++ js/markers.js
@@ -645,210 +645,210 @@ var markers = [
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXIII - Req: None - inside a small cave",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1481,2757]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure V - Req: Lockpick (Very Hard) - next to a broken hut near the deer hunting spot",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2244,2585]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXIV - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1636,2569]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure III - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [1585,2562]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XX - Req: Lockpick (Easy) - hidden in high grass right next to the western tree",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1863,2379]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXV - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2620,2047]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure IX - Req: None - right next to the stone bridge",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1687,1766]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXI - Req: Lockpick (Very Hard) - enter the cave to the northeast of this chest, stick to the left wall and climb up 3 ladders",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1988,1567]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVIII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [641,1107]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVI - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2244,754]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVII - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1830,741]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure V - Req: None - bag in a dried-up well",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [58,9]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure I - Req: Lockpick (Hard) (use the ladder in the westernmost house)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2020,-178]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure II - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2384,-336]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure III - Req: Lockpick (Very Hard) - behind an old house",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2614,-502]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VI - Req: Lockpick (Hard)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [317,-654]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-268,-757]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure IV - Req: Lockpick (Very Hard)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [1047,-862]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIX - Req: Spade - grave on a little island on the river",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1698,-1015]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XV - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [1281,-1026]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIV - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [2346,-1129]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XI - Req: None",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-865,-1299]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VIII - Req: None - bag behind a bare tree",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [406,-1315]
   } ,
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure X - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-523,-1589]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure I - Req: Spade",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [2612,-1672]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure IV - Req: Lockpick (Easy)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-469,-1815]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [2240,-1903]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXII - Req: None - bag next to destroyed gallows",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-2072,-2054]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XII - Req: Lockpick (Hard)",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [1619,-2455]
   },
   {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure II - Req: None - look for a big rock next to a cave",
     "group": "treasure_chest",
-    "icon": iconsUrl+"treasure_chest.png",
+    "icon": "treasure_chest",
     "coords": [-1685,-2545]
   },
 
