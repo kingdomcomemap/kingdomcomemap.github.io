235dd5e20fcb23ffe40ac6b469b97267b57717c2
POiZiE <37002790+POiZiE@users.noreply.github.com>
2018-03-15 16:20:42 +0100

    Update usr_markers.js

--- js/usr_markers.js
+++ js/usr_markers.js
@@ -31,7 +31,6 @@ var usr_markers =
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [3155,3838],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XX",
@@ -61,174 +60,322 @@ var usr_markers =
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [870,3282],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXI - Req: Lockpick (Very Hard) - enter the cave to the northeast of this chest, stick to the left wall and climb up 3 ladders",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [659,3143],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVIII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2496,2821],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVI - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [481,2575],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XVII - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [770,2566],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure V - Req: None - bag in a dried-up well",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2089,2054],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure I - Req: Lockpick (Hard) (use the ladder in the westernmost house)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [637,1924],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure II - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [383,1813],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure III - Req: Lockpick (Very Hard) - behind an old house",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [222,1697],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VI - Req: Lockpick (Hard)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2269,1591],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [1861,1519],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure IV - Req: Lockpick (Very Hard)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2779,1446],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIX - Req: Spade - grave on a little island on the river",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [862,1339],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XV - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2943,1331],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIV - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [3687,1259],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XI - Req: None",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [1444,1141],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure VIII - Req: None - bag behind a bare tree",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [2332,1129],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure X - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [1683,938],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure I - Req: Spade",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [3872,880],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure IV - Req: Lockpick (Easy)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [1720,780],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XIII - Req: Lockpick (Easy)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [3613,719],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XXII - Req: None - bag next to destroyed gallows",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [601,613],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Treasure XII - Req: Lockpick (Hard)",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [3179,333],
-
 }, {
     "name": "<span data-i18n='treasure_chest'>Treasure Chest</span>",
     "desc": "Ancient Treasure II - Req: None - look for a big rock next to a cave",
     "group": "treasure_chest",
     "icon": "treasure_chest",
     "coords": [871,270],
-
 }, {
+/*
+
+	// --TREASURE MAPS--
+
+*/
     "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
     "desc": "Treasure Map I",
     "desc2": "In a mill on a barrel after climbing up a ladder",
     "group": "treasure_map",
     "icon": "treasure_map",
     "coords": [1397,1446],
-
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map II",
+    "desc2": "Once entering the south gate of the Monastery head up the stairs to the left and keep going until you get to a lavish bedroom in a restricted area; the map is on a table next to the bed",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [940,1640],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map III",
+    "desc2": "Can be bought from the Fresco Master (you won't be able to talk to him until later in the game)",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [901,1422],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map IV",
+    "desc2": "Bought from Brother Nicodemus",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [902,1683],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map V",
+    "desc2": "Bought from Miller Woyzeck",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2821,1234],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map VI",
+    "desc2": "Bought from one of the Wayfarers you can only encounter while fast-travelling",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [1973,2048],                                           // -! WAYFARER MAP !-
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map VII",
+    "desc2": "In the Monastery's library, at the nothern wall on top of a stack of books",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [967,1641],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map VIII",
+    "desc2": "Enter the windmill and head up the stairs to find it on a wooden beam",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2743,3695],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map IX",
+    "desc2": "On a table in a small hut",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [421,3559],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map X",
+    "desc2": "In a locked chest (easy) in the north-eastern most stone building",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2864,1937],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XI",
+    "desc2": "Bought from the trader Votava",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2334,2821],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XII",
+    "desc2": "Bought from Miller Peshek",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2459.114258,704.810425],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XIII",
+    "desc2": "Bought from the Herbalist",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [3247,1522],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XIV",
+    "desc2": "Bought from one of the Wayfarers you can only encounter while fast-travelling",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2023,2048],                                           // -! WAYFARER MAP !-
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XV",
+    "desc2": "Take the stairs, enter the first door into the first floor; at the end of the corridor take the door to the left and look at the top of the wardrobe for the map",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2575,445],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XVI",
+    "desc2": "Bought from the Herbalist",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [945,2340],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XVII",
+    "desc2": "Bought from Huntsman Berthold",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2629,646],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XVIII",
+    "desc2": "Bought from Huntsman Nicholas",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2444.702881,2643.914063],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XIX",
+    "desc2": "Bought from one of the Wayfarers you can only encounter while fast-travelling",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2073,2048],                                           // -! WAYFARER MAP !-
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XX",
+    "desc2": "Bought from the Herbalist Charlatan",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [906,1204],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XXI",
+    "desc2": "In an open barn on a barrel (to pick it up you might have to jump onto the bucket next to it)",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [396,3090],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XXII",
+    "desc2": "Head up the stairs right next to the tailor; go through the first two doors and take a right to go up more stairs though another door (hard) and take the next door to the left (very hard); the map is on the left hand side on the bottom shelf of the bookcase",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2517,551],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XXIII",
+    "desc2": "Climb the ladder that is leaning against the farmhouse; it's on a barrel on the right hand side",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [3530,2607],
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XXIV",
+    "desc2": "Bought from one of the Wayfarers you can only encounter while fast-travelling",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2123,2048],                                           // -! WAYFARER MAP !-
+}, {
+    "name": "<span data-i18n='treasure_map'>Treasure Map</span>",
+    "desc": "Treasure Map XXV",
+    "desc2": "Enter the inn and take a left; go down the other small set of stairs to open the door (easy) to the cellar; as you enter, the map is on a barrel at the right hand side",
+    "group": "treasure_map",
+    "icon": "treasure_map",
+    "coords": [2907,745],
 }]
